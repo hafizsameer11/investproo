9348f28c11484ec1125351b1d67bcaf71adac0bd
hafiz sameer <hmstech11@gmail.com>
2025-10-01 16:49:12 +0500

    hel

--- database/migrations/2025_10_01_100346_add_unique_constraint_to_investments_table.php
+++ database/migrations/2025_10_01_100346_add_unique_constraint_to_investments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('investments', function (Blueprint $table) {
-            $table->unique(['user_id', 'status'], 'user_active_investment_unique');
+            // $table->unique(['user_id', 'status'], 'user_active_investment_unique');
         });
     }
 
